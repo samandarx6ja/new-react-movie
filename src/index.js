bf1330e4f27f6f60a278a557f753510186d74790
Abdulaziz <73533363+Abdulaziz168@users.noreply.github.com>
2022-03-13 20:08:31 +0500

    first docs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ClassApp from './Component/ClassApp';
+
+const numbers = [1,2,3,4,5,6]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App numbers={numbers}/>
+  ,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
